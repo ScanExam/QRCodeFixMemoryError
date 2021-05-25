2f93d9bd1fe13eedcbb4f32ed14a4db9a965073b
Ailkk <romaincaruana@hotmail.com>
2021-05-25 11:29:36 +0200

    trying a new way to proceed

--- src/main/java/QRCodeLivrable/QRCodeBasique/ReaderThreadManager.java
+++ src/main/java/QRCodeLivrable/QRCodeBasique/ReaderThreadManager.java
@@ -36,7 +36,7 @@ class ReaderThreadManager extends Thread implements Runnable {
 				 * TODO
 				 * PDFBox n'est pas threadsafe
 				 * Il faudrait essayer de lire un miniDoc par thread
-				 * Afin que les threads ne se partagent pas le m�me doc
+				 * Afin que les threads ne se partagent pas le m�me doc
 				 */
 				Path temp = Files.createTempFile("miniDoc"+i, ".pdf");
 				pathsToMiniDocs.add(temp);
@@ -51,6 +51,86 @@ class ReaderThreadManager extends Thread implements Runnable {
 			splittedDocument = null;
 			
 			System.gc();
+
+			System.out.println(pathsToMiniDocs.size()/4);
+			
+			int j = 0;
+			while (j < pathsToMiniDocs.size()/4) {
+				System.out.println(j + "ème tour de boucle");
+				PDDocument miniDoc1 = PDDocument.load(pathsToMiniDocs.get((j*4)).toFile());
+				PDDocument miniDoc2 = PDDocument.load(pathsToMiniDocs.get((j*4)+1).toFile());
+				PDDocument miniDoc3 = PDDocument.load(pathsToMiniDocs.get((j*4)+2).toFile());
+				PDDocument miniDoc4 = PDDocument.load(pathsToMiniDocs.get((j*4)+3).toFile());
+				
+				ExecutorService service = Executors.newFixedThreadPool(4);
+				CountDownLatch latchThreads = new CountDownLatch(4);
+				
+				PDFRenderer pdf1 = new PDFRenderer(miniDoc1);
+				PDFRenderer pdf2 = new PDFRenderer(miniDoc2);
+				PDFRenderer pdf3 = new PDFRenderer(miniDoc3);
+				PDFRenderer pdf4 = new PDFRenderer(miniDoc4);
+				
+				service.execute(new ReaderThread(reader, 0, miniDoc1.getNumberOfPages(), pdf1, latchThreads));
+				service.execute(new ReaderThread(reader, 0, miniDoc2.getNumberOfPages(), pdf2, latchThreads));
+				service.execute(new ReaderThread(reader, 0, miniDoc3.getNumberOfPages(), pdf3, latchThreads));
+				service.execute(new ReaderThread(reader, 0, miniDoc4.getNumberOfPages(), pdf4, latchThreads));
+				
+				try {
+					latchThreads.await();
+				} catch (InterruptedException e) {
+					System.out.println("PEUT PAS FERMER LES THREADS");
+				}
+				service.shutdown();
+				
+				miniDoc1.close();
+				miniDoc2.close();
+				miniDoc3.close();
+				miniDoc4.close();
+				
+				System.gc();
+				
+				System.out.println("\n\n\nThread " + j + " fermé\n\n\n");
+				
+				j++;
+			}
+			
+			for(int k = j*4; k <pathsToMiniDocs.size(); k++) {
+				System.out.println("Dernière ligne droite");
+				ExecutorService service = Executors.newFixedThreadPool(1);
+				CountDownLatch latchThreads = new CountDownLatch(1);
+				
+				PDDocument miniDoc = PDDocument.load(pathsToMiniDocs.get(k).toFile());
+				
+				PDFRenderer pdf = new PDFRenderer(miniDoc);
+				
+				service.execute(new ReaderThread(reader, 0, miniDoc.getNumberOfPages(), pdf, latchThreads));
+				
+				try {
+					latchThreads.await();
+				} catch (InterruptedException e) {
+					System.out.println("PEUT PAS FERMER LES THREADS");
+				}
+				service.shutdown();
+				
+				miniDoc.close();
+				
+				System.gc();
+				
+				System.out.println("Near to close");
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			/*
 			for(int j = 0; j < pathsToMiniDocs.size(); j++) {
 				
 				PDDocument miniDoc = PDDocument.load(pathsToMiniDocs.get(j).toFile());
@@ -76,7 +156,7 @@ class ReaderThreadManager extends Thread implements Runnable {
 				System.gc();
 				
 				System.out.println("\n\n\nThread " + j + " fermé\n\n\n");
-			}
+			}*/
 				
 		} catch (IOException e) {
 			System.out.println("Erreur de split");
